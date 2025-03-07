7a47a03da6bc755e3a64741eaf02c0785b5f77bb
Miss Electronics <myinternetsmartsystems@gmail.com>
2025-03-07 11:11:04 +0530

    Add files via upload

---
+++ mybackend/api/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class HelloWorld(APIView):
+    def get(self, request):
+        return Response({"message": "Hello from Django!"})
